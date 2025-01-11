456235763b23c73a114d43b4aa254488704e0e97
Shub7nb <shubhamnb02@gmail.com>
2025-01-12 01:24:41 +0530

    aba

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Signup } from './pages/Signup'
-import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
-import { Blogs } from "./pages/Blogs";
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Import Navigate
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Blog } from './pages/Blog';
+import { Blogs } from './pages/Blogs';
 import { Publish } from './pages/Publish';
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* Redirect the root route to /signin */}
+          <Route path="/" element={<Navigate to="/signin" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
@@ -19,7 +20,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
